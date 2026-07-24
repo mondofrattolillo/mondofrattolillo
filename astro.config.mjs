5439517ffae61715358c5cc289aa33d9e145e058
mondofrattolillo <faridamarco@gmail.com>
2026-07-24 19:17:40 +0200

    Setup Astro + Storyblok + blog Markdown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,43 +1,73 @@
 // @ts-check
-
+import { defineConfig, fontProviders, envField } from 'astro/config';
+import { env } from 'node:process';
+import node from '@astrojs/node';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import { defineConfig, fontProviders } from 'astro/config';
+import { storyblok } from '@storyblok/astro';
+import basicSsl from '@vitejs/plugin-basic-ssl';
+
+const isDev = process.env.NODE_ENV !== 'production';
 
 // https://astro.build/config
 export default defineConfig({
-        site: 'https://example.com',
-        integrations: [mdx(), sitemap()],
-        image: { service: { entrypoint: 'astro/assets/services/noop' } },
-        markdown: {
-                remarkPlugins: [],
-                rehypePlugins: [],
-        },
-        experimental: {
-                contentIntellisense: false,
-        },
-	fonts: [
-		{
-			provider: fontProviders.local(),
-			name: 'Atkinson',
-			cssVariable: '--font-atkinson',
-			fallbacks: ['sans-serif'],
-			options: {
-				variants: [
-					{
-						src: ['./src/assets/fonts/atkinson-regular.woff'],
-						weight: 400,
-						style: 'normal',
-						display: 'swap',
-					},
-					{
-						src: ['./src/assets/fonts/atkinson-bold.woff'],
-						weight: 700,
-						style: 'normal',
-						display: 'swap',
-					},
-				],
-			},
-		},
-	],
+  site: 'https://example.com',
+  output: isDev ? 'server' : 'static',
+  adapter: isDev ? node({ mode: 'standalone' }) : undefined,
+  integrations: [
+    mdx(),
+    sitemap(),
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      apiOptions: {
+        region: 'eu',
+      },
+      livePreview: true,
+      components: {
+        page: 'storyblok/Page',
+        articolo: 'storyblok/Articolo',
+        feature: 'storyblok/Feature',
+        grid: 'storyblok/Grid',
+        teaser: 'storyblok/Teaser',
+      },
+    }),
+  ],
+  image: { service: { entrypoint: 'astro/assets/services/noop' } },
+  markdown: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
+  experimental: {
+    contentIntellisense: false,
+  },
+  fonts: [
+    {
+      provider: fontProviders.local(),
+      name: 'Atkinson',
+      cssVariable: '--font-atkinson',
+      fallbacks: ['sans-serif'],
+      options: {
+        variants: [
+          {
+            src: ['./src/assets/fonts/atkinson-regular.woff'],
+            weight: 400,
+            style: 'normal',
+            display: 'swap',
+          },
+          {
+            src: ['./src/assets/fonts/atkinson-bold.woff'],
+            weight: 700,
+            style: 'normal',
+            display: 'swap',
+          },
+        ],
+      },
+    },
+  ],
+  vite: {
+    plugins: isDev ? [basicSsl()] : [],
+    server: {
+      https: isDev,
+    },
+  },
 });
